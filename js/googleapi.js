034be413d4507573f7bde3b7dda35913bb5833d5
MbargouG <97864057+MbargouG@users.noreply.github.com>
2022-04-21 00:10:02 -0500

    fix

--- js/googleapi.js
+++ js/googleapi.js
@@ -46,27 +46,8 @@ function initMap() {
         }
 
     //slider
-    var slider_img = document.querySelector('.slider-img');
-    
-
-    function prev(){
-      if(i <= 0) i = images.length;
-      i--;
-      return setImg();
-    }
-
-    function next(){
-      if(i >= images.length-1) i = -1;
-      i++;
-      return setImg();
-    }
-
-    function setImg(){
-      return slider_img.setAttribute('src', "resources/" + images[i]);
-
-    }
-
-    // Marker Content
+    var slider_img=document.querySelector('.slider-img');
+// Marker Content
     const iconBase =
         "https://developers.google.com/maps/documentation/javascript/examples/full/images/";
     const icons={
